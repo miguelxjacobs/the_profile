4f79f867a06fd856265f20f692cf37298d787c78
Miguel <36174997+miguelxjacobs@users.noreply.github.com>
2018-08-07 15:50:16 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let contactBtn = document.getElementById('contactBtn');
+let wrapper = document.getElementById('contact_wrapper');
+
+contactBtn.onclick = function() {
+    wrapper.style.display = "grid";
+}
+
+
+
+console.log(wrapper);
