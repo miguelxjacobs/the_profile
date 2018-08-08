405422027c218092fa2015136b4131d8415856eb
Miguel <36174997+miguelxjacobs@users.noreply.github.com>
2018-08-08 15:51:55 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,29 @@
-const close = document.getElementById('close');
+// const welcomebBg = document.getElementsByClassName('welcome')[0];
 
-let contactBtn = document.getElementById('contactBtn');
-let wrapper = document.getElementById('contact_wrapper');
+// XP button
+const xpClose = document.getElementsByClassName('close')[0];
+const xpBtn = document.getElementById('xpBtn');
+const xpWrapper = document.getElementById('xp_wrapper');
 
-contactBtn.onclick = function() {
-    wrapper.style.display = "grid";
+xpBtn.onclick = function() {
+    xpWrapper.style.display = "grid";
+    // welcomebBg.style.filter = "blur(2.5px)";
 }
 
-close.onclick = function() {
-    wrapper.style.display = "none";
+xpClose.onclick = function() {
+    xpWrapper.style.display = "none";
+    // welcomebBg.style.filter = "blur(0)";
+}
+
+// Contact button
+const contactClose = document.getElementsByClassName('close')[1];
+const contactBtn = document.getElementById('contactBtn');
+const contactWrapper = document.getElementById('contact_wrapper');
+
+contactBtn.onclick = function() {
+    contactWrapper.style.display = "grid";
+}
+
+contactClose.onclick = function() {
+    contactWrapper.style.display = "none";
 }
